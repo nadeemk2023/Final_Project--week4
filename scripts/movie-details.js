e7427e42012fa749cde480ada5f07da925c82728
nadeemk2023 <nadeemekhan@gmail.com>
2024-11-26 12:09:03 -0800

    Enhance movie details layout and styling; improve information display with structured HTML

--- scripts/movie-details.js
+++ scripts/movie-details.js
@@ -13,31 +13,50 @@ async function getMovieDetails(imdbId) {
 }
 
 function movieDetailsHTML(movie) {
+  const {
+    Title,
+    Poster,
+    Rated,
+    Released,
+    Runtime,
+    Genre,
+    Director,
+    Writer,
+    Actors,
+    Plot,
+    Language,
+    Country,
+    Awards,
+    Metascore,
+    imdbRating,
+    imdbVotes,
+    BoxOffice,
+  } = movie;
   return `<div class="movie__details--container">
         <div class="movie__details-img-container">
           <img
-            src="${movie.Poster}"
-            alt="${movie.Title} Poster"
+            src="${Poster}"
+            alt="${Title} Poster"
             class="movie__details--img"
           />
         </div>
         <div class="movie__details--info-container">
-          <h2 class="movie__details--title">${movie.Title}</h2>
-          <p class="movie__details">Rated: ${movie.Rated}</p>
-          <p class="movie__details">Released:</p>
-          <p class="movie__details">Runtime:</p>
-          <p class="movie__details">Genre:</p>
-          <p class="movie__details">Director:</p>
-          <p class="movie__details">Writer(s):</p>
-          <p class="movie__details">Actors:</p>
-          <p class="movie__details">Plot:</p>
-          <p class="movie__details">Language:</p>
-          <p class="movie__details">Country:</p>
-          <p class="movie__details">Awards:</p>
-          <p class="movie__details">Metascore:</p>
-          <p class="movie__details">IMDB Rating:</p>
-          <p class="movie__details">IMDB Votes:</p>
-          <p class="movie__details">Box Office:</p>
+          <h2 class="movie__details--title">${Title}</h2>
+          <p class="movie__details"><strong>Rated:</strong> ${Rated}</p>
+          <p class="movie__details"><strong>Released:</strong> ${Released}</p>
+          <p class="movie__details"><strong>Runtime:</strong> ${Runtime}</p>
+          <p class="movie__details"><strong>Genre:</strong> ${Genre}</p>
+          <p class="movie__details"><strong>Director:</strong> ${Director}</p>
+          <p class="movie__details"><strong>Writer(s):</strong> ${Writer}</p>
+          <p class="movie__details"><strong>Actors:</strong> ${Actors}</p>
+          <p class="movie__details movie__details--plot"><strong>Plot:</strong> ${Plot}</p>
+          <p class="movie__details"><strong>Language:</strong>${Language}</p>
+          <p class="movie__details"><strong>Country:</strong> ${Country}</p>
+          <p class="movie__details"><strong>Awards:</strong> ${Awards}</p>
+          <p class="movie__details"><strong>Metascore:</strong> ${Metascore}</p>
+          <p class="movie__details"><strong>IMDB Rating:</strong> ${imdbRating}</p>
+          <p class="movie__details"><strong>IMDB Votes:</strong> ${imdbVotes}</p>
+          <p class="movie__details"><strong>Box Office:</strong> ${BoxOffice}</p>
         </div>
       </div>`;
 }
