60832dfb60e32892ce859b86f767fba1694d7576
nadeemk2023 <nadeemekhan@gmail.com>
2024-11-26 01:06:09 -0800

    Add deferred loading for movie-details.js; implement movie details retrieval and rendering
    Began structuring movieDetailsHTML function after debugging why I was getting a null issue on selecting the innerhtml of an element

--- scripts/movie-details.js
+++ scripts/movie-details.js
@@ -2,3 +2,44 @@ const imdbId = localStorage.getItem('imdbId');
 
 const API_KEY = `473f3fe0`;
 const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&i=${imdbId}`;
+
+const movieDetailSection = document.querySelector('#movie-details');
+
+async function getMovieDetails(imdbId) {
+  const movie = await fetch(`${API_URL}`);
+  const movieJson = await movie.json();
+
+  movieDetailSection.innerHTML = movieDetailsHTML(movieJson);
+}
+
+function movieDetailsHTML(movie) {
+  return `<div class="movie__details--container">
+        <div class="movie__details-img-container">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_SX300.jpg"
+            alt="movie poster"
+            class="movie__details--img"
+          />
+        </div>
+        <div class="movie__details--info-container">
+          <h2 class="movie__details--title">${movie.Title}</h2>
+          <p class="movie__details">Rated:</p>
+          <p class="movie__details">Released:</p>
+          <p class="movie__details">Runtime:</p>
+          <p class="movie__details">Genre:</p>
+          <p class="movie__details">Director:</p>
+          <p class="movie__details">Writer(s):</p>
+          <p class="movie__details">Actors:</p>
+          <p class="movie__details">Plot:</p>
+          <p class="movie__details">Language:</p>
+          <p class="movie__details">Country:</p>
+          <p class="movie__details">Awards:</p>
+          <p class="movie__details">Metascore:</p>
+          <p class="movie__details">IMDB Rating:</p>
+          <p class="movie__details">IMDB Votes:</p>
+          <p class="movie__details">Box Office:</p>
+        </div>
+      </div>`;
+}
+
+getMovieDetails(imdbId);
